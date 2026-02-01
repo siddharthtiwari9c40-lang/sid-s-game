60870900e089c2e0f9225c9e2fd689f546770506
siddharthtiwari9c40-lang <siddharthtiwari9c40@gmail.com>
2026-02-01 21:06:59 +0530

    more pushes

--- DSA_C_program/Alpha.c
+++ DSA_C_program/Alpha.c
@@ -1,57 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
-void insert(int *p,int ms,int *s,int *f,int *r);
-int delete(int *p,int*s,int*f,int*r);
-void display(int *p, int sz);
+void get(int*p,int *n);
+void search(int*p,int n);
+void display(int*p,int n);
+void del(int *p,int *n);
 int main(){
-    int o,a[5],ms=5,re=-1,fr=-1,sz=0;
-    while(1){
-    printf("1 for insert\n2 for display\n3 for delete\n== ");
-    scanf("%d",&o);
-    if(o==1){
-        insert(a,ms,&sz,&fr,&re);
-    } else if(o==2){
-        display(a,sz);
-    }else if(o==3){
-        delete(a,&sz,&fr,&re);
-    } else{
+    int u,n;
+    printf("enter the size of arrey = ");
+    scanf("%d",&n);
+    int a[n];
+    get(a,&n);
+    printf("enter 1 for binary search\nenter 2 to delete an value\nenter 3 to display value\n== ");
+    scanf("%d",&u);
+    if(u==1){
+        search(a,n);
+    }else if(u==2){
+        del(a,&n);
+        display(a,n);
+    }else if(u==3){
+         display(a,n);
+    }else{
         exit(0);
     }
-}
+    return 0;
 }
 
-void insert(int *p,int ms,int *s,int *f,int *r){
-    int e;
-    if(*r==ms-1){
-        printf("overflow\n");
-    }else{
-        printf("enter the element= ");
-        scanf("%d",&e);
-        *r=(*r)+1;
-        *(p+(*r))=e;
-        (*s)++;
-        if((*f)==-1){
-            (*f)++;
-        }
-    }
-}
-int delete(int *p,int*s,int*f,int*r){
-    int e;
-    if(*f==-1){
-        printf("underflow\n");
-    } else{
-        e=*(p+(*f));
-        (*f)++;
-        if((*f)>(*r)){
-            *f=-1;
-            *r=-1;
-        }
+void get(int*p,int *n){
+    int a,i;
+    for(i=0;i<*n;i++){
+    printf("enter the value = ");
+    scanf("%d",(p+i));
     }
 }
 
-void display(int *p, int sz) {
+void search(int*p,int n){
+    int x,mid,l=0,h=n-1;
+    printf("enter the value to be searched = ");
+    scanf("%d",&x);
+    
+    while(l<=h){
+        mid=(l+h)/2;
+        if(x==*(p+mid)){
+            printf("found");
+            break;
+        }else if(x<*(p+mid)){
+            h=mid-1;
+        }else{
+            l=mid+1;  
+        }
+    }
+     if(l>h){
+        printf("not found");
+    
+      }  
+}
+void display(int*p,int n){
     int i;
-    for (i = 0; i < sz; i++) {
-        printf("%d\n", *(p + i));
+    for(i=0;i<n;i++){
+        printf("%d\n",*(p+i));
     }
+}
+
+void del(int *p,int *n){
+    int o,x,i;
+    printf("enter the value to be deleated = ");
+    scanf("%d",&o);
+    for(i=0;i<*n;i++){
+        if(o==*(p+i)){
+           break; 
+        }
+    }
+    for(x=i;x<*n-1;x++){
+        *(p+x)=*(p+x+1);
+    }
+    (*n)--;
 }
