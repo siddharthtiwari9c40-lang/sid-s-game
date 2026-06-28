a504c4037c79b10d14e2b94f162251e3e26acb28
siddharthtiwari9c40-lang <siddharthtiwari9c40@gmail.com>
2026-06-28 22:53:48 +0530

    we done

--- Python projects/Alpha.py
+++ Python projects/Alpha.py
@@ -6,12 +6,16 @@ class employee:
 class student:
     marks=400
     def __init__(self):
-        print(f"your marks{self.marks}")        
+        print(f"your marks {self.marks}")        
 
-class programmer(employee , student):
+class programmer(student,employee):
     company="flipkart"
     marks=300
+    def __init__(self):
+        print(f"your marks {self.marks}")
+        print(f"your company is {self.company}")
 
 a=programmer()
 b=employee()
 c=student()
+print(a.marks)
